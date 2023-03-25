0693172f3189acfea59f1f634c4a7c8e37231a10
Mihai Dima <dimamihaimail@gmail.com>
2023-03-25 08:52:38 -0700

    smalll changes

--- src/globalStyle.tsx
+++ src/globalStyle.tsx
@@ -1,8 +1,10 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
+// @ts-ignore
+import font from "../fonts/MYRIADPRO-REGULAR.woff";
 
 export default createGlobalStyle`
   @font-face {
     font-family: 'Myriad Pro Regular';
-    src: local('Myriad Pro Regular'), url('../fonts/MYRIADPRO-REGULAR.woff') format('woff');
+    src: local('Myriad Pro Regular'), url(${font}) format('woff');
   }
 `;
